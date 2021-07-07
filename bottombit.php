d4900a4eb2ee6c46574cb60002ab6832cee99312
LoganJackson35 <78570988+LoganJackson35@users.noreply.github.com>
2021-07-07 15:08:06 +1200

    Fixing index

---
+++ bottombit.php
@@ -0,0 +1,10 @@
+<?php
+    include "bottombit.php";
+?>
+         <div class="box footer">
+            Logan Jackson 2021
+         </div>    <!-- / footer -->
+    
+    </div>  <!-- / wrapper  -->
+    
+</body>        
--- index.php
+++ index.php
@@ -4,12 +4,12 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Put Content Here">
-    <meta name="keywords" content="Put keywords here">
-    <meta name="author" content="Put your name here">
+    <meta name="description" content="Book Review Database">
+    <meta name="keywords" content="books, reading, fiction, non-fiction, genre, reviews">
+    <meta name="author" content="Logan Jackson">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <title>Website Title goes here</title>
+    <title>Book Review Database</title>
     
     <!-- Edit the link below / replace with your chosen google font -->
     <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
@@ -35,31 +35,22 @@
             </div>    <!-- / logo -->
         </a>
             
-            <h1>Website Name</h1>
+            <h1>Blossum Books</h1>
         </div>    <!-- / banner -->   
         
-        <div class="box main">
-            <h2>First Page heading</h2>
-            
-            <p>
-                Sugar plum powder dessert bonbon powder lollipop lollipop muffin I love. Tiramisu dessert I love sugar plum apple pie marzipan chocolate bar brownie. Pudding tiramisu caramels biscuit tiramisu. I love gummies gummies pie sweet pudding I love I love. Donut halvah candy oat cake donut. Caramels wafer danish jelly-o.
-            </p>
-            
-            <p>
-                Jelly beans gingerbread jelly tootsie roll. Lemon drops bear claw I love tiramisu candy canes cake. Jelly-o apple pie powder tart I    love candy tiramisu. Topping liquorice cake sesame snaps brownie. Lemon drops jelly beans brownie danish I love fruitcake cotton candy danish. Dragée pastry halvah macaroon I love chocolate bar caramels liquorice. Halvah cupcake muffin fruitcake pastry I love bear claw I love jelly. Tart chocolate bar croissant danish jelly. Sugar plum powder cupcake muffin gingerbread I love sweet.
-            </p>
-            
-            <p>
-                Biscuit jelly I love jelly-o candy canes chupa chups topping lemon drops I love. Sesame snaps macaroon lemon drops carrot cake I love. Chocolate bar gingerbread chupa chups. Dessert chocolate pastry liquorice tootsie roll lemon drops sweet macaroon liquorice. I love muffin bonbon. Cupcake apple pie chocolate cake bonbon topping. Macaroon gummi bears dragée caramels donut chocolate.
-            </p>
-            
-        </div>    <!-- / main -->
-        
 
-        <div class="box footer">
-            CC yourname 20XX
-        </div>    <!-- / footer -->
-    
-    </div>  <!-- / wrapper  -->
-    
-</body>        
+        <div class="box side">
+
+        <h2>Search | <a class="side" href="showall.php">Show All</a></h2>
+        
+        <i>Type part of the title / author name if desired</i>
+
+        <hr />
+        
+        Title Search<br />
+        Author Search<br />
+        Genre Search<br /> 
+        Rating Search
+
+        </div>      <!-- / side area -->
+
